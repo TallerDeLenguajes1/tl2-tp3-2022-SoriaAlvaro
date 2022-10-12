a0832154e208b128aecd7c37e15d0fa0814d78d1
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-10-12 20:44:35 -0300

    borrando codigo innecesario

--- Cadeteria/Program.cs
+++ Cadeteria/Program.cs
@@ -3,15 +3,3 @@ using System.Collections.Generic;
 using System.Linq;
 using Class_MetodosDatos;
 
-int op;
-do
-{
-    op = MethodData.setInt("1)\n2)");
-    if(op == 1)
-    {
-        Console.Clear();
-        System.Console.WriteLine("Hola");
-        Console.ReadKey();
-        Console.Clear();
-    }
-}while(op == 1);
